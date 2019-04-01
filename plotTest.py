85faa6e7d3f088fc04917094b8a507693ee6204d
zachary davis <zchdvs232@gmail.com>
2019-03-31 23:38:42 -0700

    Attempt at queues

--- plotTest.py
+++ plotTest.py
@@ -4,22 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import serial
+import collections
 
 # Compute the x and y coordinates for points on a sine curve 
 ser = serial.Serial('COM7')
-fig, ax = plt.subplots()
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 100), ylim=(0, 1024))
+
+y = collections.deque()
 
 
 xAxis = np.arange(0, 100)
-y = np.zeros((100), dtype=int)
-line, = ax.plot(xAxis, y)
+#y = np.zeros((100), dtype=int)
+
 ser.readline() #so i dont get a chopped up serial byte
 for x in xAxis:    
-    y[x] = ser.readline() # fills y axis w data before going to dynamic graph
-    print(y[x])
-
-def displayY():
-     print(y)
+    y.appendleft(ser.readline()) # fills y axis w data before going to dynamic graph
+    #print(y[x])
+line, = ax.plot(xAxis, y)
 
 def init():  # first frame
     global line
@@ -31,17 +33,19 @@ def animate(i):
     global line
     #print("before delete")
     #displayY()
-    y = np.roll(y, 1)
-    #print("after delete")
+    y.pop()
+    print(y)
    # displayY()
-    y[99] = ser.readline()
-    #print("after append")
+    y.appendleft(ser.readline())
+    print(y)
     #displayY()
     line.set_ydata(y)  # update the data.
     return line,
 
-plt.ylim(0, 1024)
+
 plt.title("ADC data") 
+#ax.set_ylim(0, 1024, emit=True, auto=True)
+plt.yticks(np.arange(0, 1024, step=100), np.arange(0, 1024, step=100))
 
 # Plot the points using matplotlib 
 ani = animation.FuncAnimation(
